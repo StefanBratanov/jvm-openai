606f8c2a17f245cb3b0e83f9b7564e6bae6d431f
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-01-24 17:54:12 +0200

    Use builder to initialize ObjectMapper

--- src/main/java/io/github/stefanbratanov/jvm/openai/ObjectMapperSingleton.java
+++ src/main/java/io/github/stefanbratanov/jvm/openai/ObjectMapperSingleton.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 class ObjectMapperSingleton {
@@ -14,11 +15,13 @@ class ObjectMapperSingleton {
 
   static ObjectMapper getInstance() {
     if (instance == null) {
-      instance = new ObjectMapper();
-      instance.registerModule(new Jdk8Module());
-      instance.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-      instance.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-      instance.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
+      instance =
+          JsonMapper.builder()
+              .addModule(new Jdk8Module())
+              .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+              .serializationInclusion(JsonInclude.Include.NON_ABSENT)
+              .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+              .build();
     }
     return instance;
   }
