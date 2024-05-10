fc39bf2fa6506ef77823e84760dc5eab76091da4
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-05-10 15:08:01 +0100

    Fix ToolResources visibility

--- src/main/java/io/github/stefanbratanov/jvm/openai/ToolResources.java
+++ src/main/java/io/github/stefanbratanov/jvm/openai/ToolResources.java
@@ -8,36 +8,37 @@ import java.util.Optional;
 /** A set of resources that are used by the assistant's tools. */
 public record ToolResources(CodeInterpreter codeInterpreter, FileSearch fileSearch) {
 
-  record CodeInterpreter(List<String> fileIds) {}
+  public record CodeInterpreter(List<String> fileIds) {}
 
   /**
    * Either {@link #vectorStoreIds} or {@link #vectorStores} must be present, but not both of them.
    */
-  record FileSearch(Optional<String[]> vectorStoreIds, Optional<VectorStores[]> vectorStores) {
+  public record FileSearch(
+      Optional<String[]> vectorStoreIds, Optional<VectorStores[]> vectorStores) {
 
     public record VectorStores(List<String> fileIds, Map<String, String> metadata) {}
   }
 
-  static ToolResources codeInterpreterToolResources(List<String> fileIds) {
+  public static ToolResources codeInterpreterToolResources(List<String> fileIds) {
     return new ToolResources(new CodeInterpreter(fileIds), null);
   }
 
-  static ToolResources fileSearchToolResources(String... vectorStoreIds) {
+  public static ToolResources fileSearchToolResources(String... vectorStoreIds) {
     return new ToolResources(null, new FileSearch(Optional.of(vectorStoreIds), Optional.empty()));
   }
 
-  static ToolResources fileSearchToolResources(VectorStores... vectorStores) {
+  public static ToolResources fileSearchToolResources(VectorStores... vectorStores) {
     return new ToolResources(null, new FileSearch(Optional.empty(), Optional.of(vectorStores)));
   }
 
-  static ToolResources codeInterpreterAndFileSearchToolResources(
+  public static ToolResources codeInterpreterAndFileSearchToolResources(
       List<String> fileIds, String... vectorStoreIds) {
     return new ToolResources(
         new CodeInterpreter(fileIds),
         new FileSearch(Optional.of(vectorStoreIds), Optional.empty()));
   }
 
-  static ToolResources codeInterpreterAndFileSearchToolResources(
+  public static ToolResources codeInterpreterAndFileSearchToolResources(
       List<String> fileIds, VectorStores... vectorStores) {
     return new ToolResources(
         new CodeInterpreter(fileIds), new FileSearch(Optional.empty(), Optional.of(vectorStores)));
